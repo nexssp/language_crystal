40ce4d9dcafaf2df19e1f345af5770512c5df277
Marcin Polak <mapoart@gmail.com>
2020-09-06 21:17:47 +0200

    Added sudo for install /Linux

--- crystal.linux.nexss.config.js
+++ crystal.linux.nexss.config.js
@@ -1,7 +1,12 @@
 let languageConfig = Object.assign({}, require("./crystal.win32.nexss.config"));
+let sudo = "sudo ";
+if (process.getuid && process.getuid() === 0) {
+  sudo = "";
+}
+
 languageConfig.compilers = {
   crystalWSL: {
-    install: `curl -sSL https://dist.crystal-lang.org/apt/setup.sh | bash && apt install crystal`,
+    install: `${sudo}curl -sSL https://dist.crystal-lang.org/apt/setup.sh | bash && ${sudo}apt install crystal`,
     command: "crystal",
     args: "<file>",
     help: ``,
