55980f2a8670e3b9120d43427faa8f4e0a91b630
Marcin Polak <mapoart@gmail.com>
2020-09-11 17:33:40 +0200

    Added different distributions installs.

--- crystal.linux.nexss.config.js
+++ crystal.linux.nexss.config.js
@@ -15,10 +15,6 @@ languageConfig.compilers = {
 
 // If statement must be here for older versions nexss <2.1.12
 if (require("fs").existsSync(`${process.env.NEXSS_SRC_PATH}/lib/osys.js`)) {
-  languageConfig.languagePackageManagers.composer.installation = `php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');"
-php -r "if (hash_file('sha384', 'composer-setup.php') === '8a6138e2a05a8c28539c9f0fb361159823655d7ad2deecb371b04a83966c61223adc522b0189079e3e9e277cd72b8897') { echo 'Installer verified'; } else { echo 'Installer corrupt'; unlink('composer-setup.php'); } echo PHP_EOL;"
-php composer-setup.php
-php -r "unlink('composer-setup.php');"`;
   const { dist, version } = require(`${process.env.NEXSS_SRC_PATH}/lib/osys`);
   const distName = dist();
   // TODO: Later to cleanup this config file !!
